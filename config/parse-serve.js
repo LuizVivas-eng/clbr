18f75f1e94f86fb2f9c0e18537b26ae0c1c4ff7a
LuizVivas-eng <luizvivas9@gmail.com>
2024-11-05 08:29:22 -0300

    Alteração keys Parse-Server

--- config/parse-serve.js
+++ config/parse-serve.js
@@ -4,9 +4,14 @@ const url = 'https://parseapi.back4app.com/functions/'
 /* Homologação */
 /* const url = 'http://localhost:1337/parse/functions/' */
 
-const headers = {
+/* const headers = {
     'X-Parse-Application-Id': '44WStoAg94jkeW5hAO8hh886skVlXBqSs2I2RpjE',
     'X-Parse-REST-API-Key': 'y3kTuzTkV8oJSoxeeeyY0eRKYyyeAal7IofJxSo9',
+    'Content-Type': 'application/json'
+} */
+const headers = {
+    'X-Parse-Application-Id': '37unc06ftY1Oehoo5WAcVMzbGW9ygRzNi09wLe8r',
+    'X-Parse-REST-API-Key': 'lNejbv8CkBDqBnHy3pFZD9nwcgGEyBtTVc7s7zvX',
     'Content-Type': 'application/json'
 }
 
